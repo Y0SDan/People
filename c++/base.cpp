1004b92ab7d0eda0694d352efb2ae6a76dd9f714
Y0SDan <73656015+Y0SDan@users.noreply.github.com>
2023-01-09 13:50:36 -0600

    todavia falta

--- c++/base.cpp
+++ c++/base.cpp
@@ -1,1 +1,37 @@
+//ejemplo funciones virtuales
 #include <iostream>
+using namespace std,
+
+class base {
+public:
+    virtual void quien(){
+        cout << "base\n";
+    }
+};
+
+class primera: public base{
+public:
+    void quien(){
+        cout << "primera\n";
+    }
+};
+
+class segunda: public base{
+public:
+    void quien(){
+        cout << "segunda\n";
+    }
+};
+
+class tercera: public base{
+
+};
+
+class cuarta: public base{
+public:
+    //No se vale con un tipo de dato diferente
+    /*int quien(){
+        cout << "cuarto\n";
+        return 1;
+    }*/
+}
